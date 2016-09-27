91dcc838304a03142fccdf931736b9a32e5f7f7f
samwu <1322673636@qq.com>
2016-09-27 09:37:15 +0800

    Source:Code:add message compress function
    Signed-off-by: samwu <1322673636@qq.com>

--- serial_test_scl/Uart.py
+++ serial_test_scl/Uart.py
@@ -26,7 +26,6 @@ cmd_DeviceInfo = cmd_DeviceInfo.decode("hex")
 cmd_Systick = "5C2E00000000002ECA"
 cmd_Systick = cmd_Systick.decode("hex")
 
-
 def scan():
     """scan for available ports. return a list of tuples (num, name)"""
     available = []
@@ -44,7 +43,6 @@ def discovery_uart():
     for n,s in scan():
         print "(%d) %s" % (n,s)
 
-		
 def uart_change_status(x):
 	global status
 	status = x
@@ -64,64 +62,70 @@ def uart_add_char_to_pbuf(char):
 	global printf_str
 	printf_str = printf_str + ' '+char
 
-def str_2_hex_to_int(str):
-	sStr1 = str[0:2]
-	num = string.atoi(sStr1, 16)
-	print num
+def uart_xor_cal(x):
+	global uart_xor
+	#print "uart_xor_cal input data :",hex(ord(x))
+	uart_xor = uart_xor ^ ord(x)
+	#print "uart_xor_cal ouput data :",hex(uart_xor)
 
-def str_1_hex_to_2_hex(str):
-	sStr1 = str
+def uart_change_xor(x):
+	global uart_xor
+	uart_xor = x
+
+def uart_get_xor():
+	global uart_xor
+	return uart_xor
+
 	
-	sStr2 = sStr1.split(' 0')
-	delimiter = ' 00'
-	sStr2 = delimiter.join(sStr2)
-	return sStr2
-
-
 def uart_show_message(str):
-	sStr1 = str
+	print "Message->HEADER =",str[1:3]
+	print "Message->TYPE   =",str[4:6]
 	
-	sStr2 = sStr1.split(' 0')
-	delimiter = ' 00'
-	sStr2 = delimiter.join(sStr2)
-	
-	print sStr2
-	
-	print "Message->header =",sStr2[1:3]
-	print "Message->Type   =",sStr2[4:6]
-	
-	sign_str = sStr2[7:18]
+	sign_str = str[7:18]
 	print "Message->SIGN   =",sign_str
 	
-	len_str = sStr2[19:21]
+	len_str = str[19:21]
 	print "Message->LEN    =",len_str
 	len_int = string.atoi(len_str, 16)
-	print len_int
+	#print len_int
 	
-	data = sStr2[22:22+len_int*3]
-	print "Message->DATA    = ",data
-	
-	xor = sStr2[222+len_int*3+1:22+len_int*3+3]
-	print "Message->OXR    = ",xor
-	
-	
-def uart_clear_pbuf():
-	global printf_str
-	print printf_str
-	uart_show_message(printf_str)
-	printf_str = ""
-	
-def uart_change_test_cnt(data):
-	global test_cnt
-	test_cnt = test_cnt + data
-	print "uart_change_test_cnt:",test_cnt
+	data = str[22:22+len_int*3]
+	print "Message->DATA   =",data
 
+	Message_xor = str[22+len_int*3:22+len_int*3+2]
+	print "Message->OXR    =",Message_xor
+
+	Message_end = str[22+(len_int+1)*3:22+(len_int+1)*3+2]
+	print "Message->END    =",Message_end
+	
+def uart_change_test_cnt_ok(data):
+	global test_cnt_ok
+	test_cnt_ok = test_cnt_ok + data
+	print "uart_change_test_cnt_ok:",test_cnt_ok
+
+def uart_change_test_cnt_err(data):
+	global test_cnt_err
+	test_cnt_err = test_cnt_err + data
+	print "uart_change_test_cnt_err:",test_cnt_err
+	
+def uart_clear_pbuf(x):
+	global printf_str
+	#print printf_str
+	if x == 0:
+		uart_show_message(printf_str)
+		printf_str = ""
+		uart_change_test_cnt_ok(1)
+	else:
+		printf_str = ""
+		uart_change_test_cnt_err(1)
 	
 def uart_decode_machine(x):
 	global status
 	global uart_cnt
+	global uart_xor
 	
-	char = hex(ord(x))[2:]
+	char = "%02x" % ord(x)
+	#print char
 
 	# revice header
 	if status == 0:
@@ -133,6 +137,7 @@ def uart_decode_machine(x):
 	# revice cmd type
 	if status == 1:
 		uart_add_char_to_pbuf(char)
+		uart_xor_cal(x)
 		uart_change_status(2)
 		uart_change_uart_cnt(4)
 		return
@@ -140,7 +145,8 @@ def uart_decode_machine(x):
 	# rvice sign id
 	if status == 2:
 		uart_add_char_to_pbuf(char)
-		uart_change_uart_cnt_dec() 
+		uart_xor_cal(x)
+		uart_change_uart_cnt_dec()
 		if uart_cnt == 0:
 			uart_change_status(3)
 		return
@@ -148,36 +154,61 @@ def uart_decode_machine(x):
 	# revice message data len
 	if status == 3:
 		uart_add_char_to_pbuf(char)
+		uart_xor_cal(x)
 		if ord(x) == 0:
 			uart_change_status(5)
 			return
 		uart_change_status(4)
 		uart_change_uart_cnt(ord(x))
-		print "message len = ",ord(x)
+		#print "message len = ",ord(x)
 		return
 		
 	# revoce data
 	if status == 4:
 		uart_add_char_to_pbuf(char)
+		uart_xor_cal(x)
 		uart_change_uart_cnt_dec() 
 		if uart_cnt == 0:
 			uart_change_status(5)
 		return
 	
+	# revoce xor
+	if status == 5:
+		uart_cal_oxr = uart_get_xor()
+		uart_cal_oxr = "%02x" % uart_cal_oxr
+		#print "uart_revice_oxr =",char
+		#print "uart_xor_cal(x) =",uart_cal_oxr
+		uart_oxr_cmp = cmp(uart_cal_oxr,char)
+		print uart_oxr_cmp
+		if uart_oxr_cmp == 0:
+			uart_add_char_to_pbuf(char)
+			uart_change_status(6)
+			uart_change_xor(0)
+			return
+		else:
+			uart_change_status(0)
+			uart_change_xor(0)
+			# uart xor data err
+			uart_clear_pbuf(1)
+			return
+
 	# revice data end
-	if status == 5:	
+	if status == 6:
 		if char == "ca":
 			uart_change_status(100)
 			uart_add_char_to_pbuf(char)
-			uart_clear_pbuf()
+			# uart xor data ok
+			uart_clear_pbuf(0)
 			return
 
-	
+
 if __name__=='__main__':
 	status = 0
 	printf_str = ""
 	uart_cnt = 0
-	test_cnt = 0
+	test_cnt_ok = 0
+	test_cnt_err = 0
+	uart_xor = 0
 	
 	discovery_uart();
 #	selport = input('please select port:')
@@ -194,6 +225,7 @@ if __name__=='__main__':
 	while True: 
 		# send cmd meaasge
 		ser.write(cmd_DeviceInfo)
+		ser.write(cmd_Systick)
 		
 		# decode return message 
 		while True:
@@ -204,7 +236,6 @@ if __name__=='__main__':
 				break
 				
 		# Statistical time 
-		uart_change_test_cnt(1)		
 		endTime = time.time()
 		print "use time: "+str(endTime-startTime)
 		
