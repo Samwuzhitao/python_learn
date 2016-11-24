9a20553223c2274c27a4a81a331a53c75837a411
Samwuzhitao <1322673636@qq.com>
2016-11-24 10:46:28 +0800

    Analisys:Source:Modify statistic logic
    Signed-off-by: Samwuzhitao <1322673636@qq.com>

--- Analisys/Analysis-procotol-v2.0.py
+++ Analisys/Analysis-procotol-v2.0.py
@@ -24,6 +24,7 @@ class TestFile():
 		self.lostflg      = 0
 		self.lostuid      = ""
 		self.analysispath = ""
+		self.startindex   = 0
 		f = open(file_name,'rU')
 		self.filelines  = f.readlines()
 		f.close()
@@ -37,13 +38,13 @@ class TestFile():
 		f.close()
 
 	def set_count_num(self,str1):
-		if ((str1[0:1]  != "[") & (str1[2:3] == "1")):
+		if ((str1[0:5]  == "First")):
 			self.count = 1
-		if ((str1[0:1]  != "[") & (str1[2:3] == "2")):
+		if ((str1[0:6]  == "Second")):
 			self.count = 2
-		if ((str1[0:1]  != "[") & (str1[2:3] == "3")):
+		if ((str1[0:5]  == "Third")):
 			self.count = 3
-		if ((str1[0:1]  != "[") & (str1[2:3] == "4")):
+		if ((str1[0:6]  == "Fourth")):
 			self.count = 4
 
 	def show_lost_uid(self,str1):
@@ -95,10 +96,15 @@ class TestFile():
 			self.result_check(str1,3)
 			self.result_check(str1,4)
 
+		if str[0:9]  == "Statistic":
+			self.lineindex = string.atoi(str[11:])
+			if self.startindex == 0: 
+				self.startindex = self.lineindex
+
 		if str[0:3]  == "Sum":
-			self.str ="<%03d>" % self.lineindex + self.str + " Sum:" + str1[10:13] +", "
+			self.str ="<%05d>" % self.lineindex + self.str + " Sum:%3s" % str1[10:13] +", "
 			#self.str ="<%03d>" % self.lineindex
-			self.lineindex = self.lineindex+1
+			#self.lineindex = self.lineindex+1
 			self.count = 0
 			if self.sumcount != 0:
 				show_str = self.str + "lost uid: " + self.lostuid
@@ -118,6 +124,9 @@ class TestFile():
 
 if __name__=='__main__':
 	# get uart configuration
+	#num = raw_input("Clicker Num :\r\n>>>")
+	#delayms = raw_input("Clicker delay ms :\r\n>>>")
+
 	path = os.path.abspath("./")
 
 	# get the cmd num of the file 'testfile.txt'
@@ -134,23 +143,25 @@ if __name__=='__main__':
 
 	show_str = "Test statistical result:"
 	tf.show(show_str,'a')
-	show_str = "Sum     count : %2d" % tf.lineindex
+	show_str = "Time          : %d min" % ((tf.lineindex-tf.startindex)*4.0/60)
+	tf.show(show_str,'a')
+	show_str = "Sum     count : %6d" % (tf.lineindex-tf.startindex)
 	tf.show(show_str,'a')
 	ok_count = tf.ok[0] + tf.ok[1] + tf.ok[2] + tf.ok[3]
-	show_str = "Success count : %2d" % ok_count + "  < %7.3f %% >" % (ok_count*100.0 / tf.lineindex)
+	show_str = "Success count : %6d" % ok_count + "  < %7.3f %% >" % (ok_count*100.0 / (tf.lineindex-tf.startindex))
 	tf.show(show_str,'a')
-	one_ok_rate   = "  < %7.3f %% >" % (tf.ok[0]*100.0/tf.lineindex)
-	show_str = "One     count : %2d" % tf.ok[0]   + one_ok_rate
+	one_ok_rate   = "  < %7.3f %% >" % (tf.ok[0]*100.0/(tf.lineindex-tf.startindex))
+	show_str = "One     count : %6d" % tf.ok[0]   + one_ok_rate
 	tf.show(show_str,'a')
-	two_ok_rate   = "  < %7.3f %% >" % ((tf.ok[0] + tf.ok[1])*100.0/tf.lineindex)
-	show_str = "Two     count : %2d" % (tf.ok[0] + tf.ok[1])   + two_ok_rate
+	two_ok_rate   = "  < %7.3f %% >" % ((tf.ok[1])*100.0/(tf.lineindex-tf.startindex))
+	show_str = "Two     count : %6d" % (tf.ok[1])   + two_ok_rate
 	tf.show(show_str,'a')
-	three_ok_rate = "  < %7.3f %% >" % ((tf.ok[0] + tf.ok[1] + tf.ok[2])*100.0/tf.lineindex)
-	show_str = "Three   count : %2d" % (tf.ok[0] + tf.ok[1] + tf.ok[2]) + three_ok_rate
+	three_ok_rate = "  < %7.3f %% >" % ((tf.ok[2])*100.0/(tf.lineindex-tf.startindex))
+	show_str = "Three   count : %6d" % (tf.ok[2]) + three_ok_rate
 	tf.show(show_str,'a')
-	four_ok_rate  = "  < %7.3f %% >" % ((tf.ok[0] + tf.ok[1] + tf.ok[2] + tf.ok[3])*100.0/tf.lineindex)
-	show_str = "Four    count : %2d" % (tf.ok[0] + tf.ok[1] + tf.ok[2] + tf.ok[3])  + four_ok_rate
+	four_ok_rate  = "  < %7.3f %% >" % ((tf.ok[3])*100.0/(tf.lineindex-tf.startindex))
+	show_str = "Four    count : %6d" % (tf.ok[3])  + four_ok_rate
 	tf.show(show_str,'a')
-	fail_rate     = "  < %7.3f %% >" % ((tf.lineindex - ok_count)*100.0/tf.lineindex)
-	show_str = "Fail    count : %2d" % (tf.lineindex - ok_count) + fail_rate
+	fail_rate     = "  < %7.3f %% >" % ((tf.lineindex - tf.startindex - ok_count)*100.0/(tf.lineindex-tf.startindex))
+	show_str = "Fail    count : %6d" % (tf.lineindex - tf.startindex - ok_count) + fail_rate
 	tf.show(show_str,'a')
