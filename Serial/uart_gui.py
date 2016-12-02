985403c1d6bef8a40b7f79552ea745f6f3ad0289
samwu <1322673636@qq.com>
2016-12-02 18:31:22 +0800

    Serial:Add GUI
    Signed-off-by: samwu <1322673636@qq.com>

--- Serial/uart_gui.py
+++ Serial/uart_gui.py
@@ -11,25 +11,69 @@ import serial
 import threading
 import Tkinter as Tk
 import Tkinter as tk
+import string
 
+import uart_init
+import uart_decode
+import uart_message
 
 isOpened = threading.Event()
 
-RBuf = ''
-TBuf = ''
-
 root = Tk.Tk()
 ComX = Tk.StringVar(root,'COM1')
-Baud = Tk.StringVar(root,"9600")
+Baud = Tk.StringVar(root,"1152000")
 Dbit = Tk.StringVar(root,'8')
 Sbit = Tk.StringVar(root,'1')
 Chck = Tk.StringVar(root,'None')
 HexD = Tk.BooleanVar(root,False)
 HexO = Tk.BooleanVar(root,False)
 Open = Tk.StringVar(root,u'打开串口')
+availableCom = uart_init.guiscan();
+SerialPort = 1
+txt1 = 1
 
+def printCoords():
+    print "hello word"
+
+def gui_show(str,mode):
+    global txt1
+    txt1.insert("insert",str+"\r\n")
+
+def uart_compress_cmd_process():
+    global SerialPort
+    global uartr
+    global uartm
+    global txt1
+    char = ""
+
+    while True:
+        read_char = SerialPort.read(1)
+        #print read_char
+        char += "%02X " % ord(read_char)
+        uartr.revice_status_machine(read_char,uartm.message_show,uartm.count_inc)
+        uart_current_status = uartr.get_status()
+        if uart_current_status == 100:
+            cmd_type = char[3:5]
+            len_str  = char[18:20]
+            len_int  = string.atoi(len_str, 16)
+            data     = char[21:21+len_int*3]
+
+            uartm.ReviceFunSets[cmd_type](len_int,data,gui_show)
+
+            char = ""
+            uartr.set_status(0)
+
+def uart_send_data():
+    global SerialPort
+    uart_cmd_data = "5C 10 01 0C 14 55 0D 5A 00 00 00 00 00 11 03 01 01 41 53 CA C1 CA"
+    uart_cmd_data = uart_cmd_data.replace(' ','')
+    print uart_cmd_data
+    uart_cmd_data = uart_cmd_data.decode("hex")
+    SerialPort.write(uart_cmd_data)
 
 def main():
+    global txt1
+
     root.title("COM Shell")
 
     txt1 = Tk.Text(root,width=80,border=5)
@@ -38,17 +82,19 @@ def main():
     cnv1 = tk.Canvas(root,height=26,width=580)
     cnv1.pack(side='top',padx=0,pady=0,anchor='c')
     cnv1.create_window( 30,15,window=ttk.Label(root,text=u'输入框：'))
-    cnv1.create_window(210,15,window=ttk.Entry(root,width=44))
-    cnv1.create_window(387,15,window=ttk.Button(root,text=u'发送',width=10))
+    cnv1.create_window(210,15,window=ttk.Entry(root,width=43))
+    send = ttk.Button(root,text=u'发送',width=10,command=uart_send_data)
+    send.pack(side='top',padx=0,pady=0,anchor='w')
+    cnv1.create_window(387,15,window=send)
     cnv1.create_window(472,15,window=ttk.Button(root,text=u'清除',width=10))
     cnv1.create_window(547,15,window=ttk.Checkbutton(root,text=u'HEX显示',variable=HexD,onvalue=True,offvalue=False))
 
     cnv2 = tk.Canvas(root,height=26,width=580)
     cnv2.pack(side='top',padx=0,pady=0,anchor='c')
     cnv2.create_window( 30,15,window=ttk.Label(root,text=u'串口号：'))
-    cnv2.create_window(105,15,window=ttk.Combobox(root,textvariable=ComX,values=['COM1', 'COM8', 'COM10','COM14'],width=12))
+    cnv2.create_window(105,15,window=ttk.Combobox(root,textvariable=ComX,values=availableCom,width=12))
     cnv2.create_window(202,15,window=ttk.Label(root,text=u'波特率：'))
-    cnv2.create_window(277,15,window=ttk.Combobox(root,textvariable=Baud,values=['4800','9600','115200'],width=12))
+    cnv2.create_window(277,15,window=ttk.Combobox(root,textvariable=Baud,values=['9600','115200','1152000'],width=12))
     cnv2.create_window(398,15,window=ttk.Button(root,textvariable=Open,width=10,command=lambda:COMOpen(cnv2)))
     cnv2.create_oval(470,7,486,23,fill='black',tag='led')
     cnv2.create_window(547,15,window=ttk.Checkbutton(root,text=u'HEX发送',variable=HexO,onvalue=True,offvalue=False))
@@ -63,55 +109,35 @@ def main():
     cnv3.create_window(445,15,window=ttk.Combobox(root,textvariable=Chck,values=['None','Odd','Even','Mark','Space'],width=12))
     cnv3.create_window(547,15,window=ttk.Button(root,text=u'扩展',width=9))
 
-    com_thread = threading.Thread(target=COMTrce)
-    com_thread.setDaemon(True)
-    com_thread.start()
-
-    root.bind("<<COMRxRdy>>",lambda e: txt1.insert("insert",RBuf))
+    cnv1.bind('<Button-1>',printCoords)
 
     root.mainloop()
 
-COM = serial.Serial()
+
 def COMOpen(cnv2):
+    global SerialPort
+
     if not isOpened.isSet():
         try:
-            COM.timeout = 1
-            COM.xonxoff = 0
-            COM.port = ComX.get()
-            COM.parity = Chck.get()[0]
-            COM.baudrate = int(Baud.get())
-            COM.bytesize = int(Dbit.get())
-            COM.stopbits = int(Sbit.get())
-            COM.open()
+            SerialPort = serial.Serial( 5, 1152000 )
         except Exception:
-            print "COM Open Error!"
+            print "Serial Open Error!"
         else:
             isOpened.set()
             Open.set(u'关闭串口')
             cnv2.itemconfig('led',fill='green')
+            reader  = threading.Thread(target=uart_compress_cmd_process)
+            print 'Process reader is going to start...'
+            reader.start()
     else:
-        COM.close()
+        SerialPort.close()
         isOpened.clear()
         Open.set(u'打开串口')
         cnv2.itemconfig('led',fill='black')
 
-def COMTrce():
-    while True:
-        if isOpened.isSet():
-            RBuf = COM.read(1)          #read one, with timout
-            if RBuf:
-                n = COM.inWaiting()
-                if n:
-                    RBuf = RBuf+COM.read(n)
-                    root.event_generate("<<COMRxRdy>>")
-
-            if len(TBuf)!=0:
-                COM.write(TBuf)
-
-            time.sleep(0.01)
-        time.sleep(0.05)
-
 
 if __name__=='__main__':
+    uartr = uart_decode.UartR()
+    uartm = uart_message.UartM()
     isOpened.clear()
     main()
--- Serial/uart_init.py
+++ Serial/uart_init.py
@@ -18,6 +18,18 @@ def scan():
             pass
     return available
 
+def guiscan():
+    """scan for available ports. return a list of tuples (num, name)"""
+    available = []
+    for i in range(256):
+        try:
+            s = serial.Serial(i)
+            available.append(s.portstr)
+            s.close()   # explicit close 'cause of delayed GC in java
+        except serial.SerialException:
+            pass
+    return available
+
 def uart_scan():
     print "Found ports:"
     for n,s in scan():
