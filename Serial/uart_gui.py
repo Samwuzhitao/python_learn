42627c670f4b392a92ad9afb6e72fe76288ecf66
samwu <1322673636@qq.com>
2017-04-26 21:23:00 +0800

    Source:Code:增加Ymodem协议，完成镜像文件的解析与发送链接的建立

--- Serial/uart_gui.py
+++ Serial/uart_gui.py
@@ -22,6 +22,115 @@ decode_type_flag = 0
 hex_decode_show_style = 1
 down_load_image_flag  = 0
 
+class UartYmodemSendFile():
+    def __init__(self): 
+        self.package    = []
+        self.pack_cmd   = 0
+        self.pack_num   = 0
+        self.pack_unnum = 0
+        self.crc16      = 0
+
+    def update_crc16(self,crc_in,u8_data):
+        crc_temp = crc_in
+        data_in  = u8_data | 0x100
+
+        while((data_in & 0x10000) == 0):
+            crc_temp <<= 1
+            data_in  <<= 1
+            if (data_in & 0x100):
+                crc_temp = crc_temp + 1
+            if (crc_temp & 0x10000):
+                crc_temp ^= 0x1021
+
+        return (crc_temp & 0xFFFF)
+
+    def cal_crc16(self,u8_data_array):
+        crc = 0
+        for i in u8_data_array:
+            crc = self.update_crc16(crc,i)
+
+        crc = self.update_crc16(crc,0)
+        crc = self.update_crc16(crc,0)
+        return (crc & 0xFFFF)
+
+    def encode_header_package(self,file_name,file_size):
+        NOP  = 0
+        data = "%s %d" % (file_name,file_size)
+
+        self.package= []
+        # 封装帧头
+        self.pack_cmd = 1
+        self.pack_num = 0
+        self.pack_unnum = 0xFF
+        self.package.append(self.pack_cmd)
+        self.package.append(self.pack_num)
+        self.package.append(self.pack_unnum)
+
+        # 封装帧内容
+        for item in data:
+             self.package.append(ord(item))
+        for i in range(128-len(data)):
+            self.package.append(NOP)
+        #计算CRC16
+        self.crc16 = self.cal_crc16(self.package[3:])
+        
+        self.package.append(self.crc16 & 0xFF)
+        self.package.append((self.crc16 & 0xFF00)>>8)
+
+        self.pack_num = self.pack_num + 1
+        self.pack_cmd = self.pack_cmd + 1
+
+        return self.package
+
+    def encode_data_package(self,data):
+        self.pack_num = self.pack_num + 1
+
+        print self.pack_num
+
+
+    def send_package(self):
+        print "send"
+
+    def run(self):
+        data_path  = os.path.abspath("./") +'\\data\\'
+        image_path = data_path + 'DTQ_RP551CPU_ZKXL0200_V0102.bin'
+        size       = os.path.getsize(image_path)
+        #print size
+        f = open(image_path, "rb")
+
+        send_index = 0
+        count = 0
+        DATA_LEN = 1024
+
+        #发送文件名和长度
+        self.encode_header_package(image_path,size)
+
+
+        while( size > send_index ):
+            #print count
+            #print "read_start = %d " % (send_index)
+            f.seek(send_index,0)
+
+            if (send_index + DATA_LEN) < size:
+                read_count = DATA_LEN
+            else:
+                read_count = size-send_index
+
+            temp=f.read(read_count) 
+            send_index = send_index + read_count
+
+            u8_array  = [string.atoi(item.encode('hex'), 16) for item in temp]
+            #for i in u8_array:
+            #    print  ",0x%02x" % i,
+            #print ""
+            crc16 = self.cal_crc16(u8_array)
+            count = count + 1
+            #print "read_count = %03d crc16 = %04x" % (count,crc16)
+
+
+        f.close()
+
+
 class UartListen(QThread): 
     def __init__(self,parent=None): 
         super(UartListen,self).__init__(parent) 
@@ -43,12 +152,24 @@ class UartListen(QThread):
         while self.working==True: 
             if ser.isOpen() == True:
                 read_char = ser.read(1)
-                if down_load_image_flag == 2:
+
+                if down_load_image_flag == 3:
                     if read_char == 'C':
-                        data = u"开始下载 image"
+                    #if down_load_image_flag == 2:
+                        data = u"建立连接,发送镜像文件信息"
+                        data_path  = os.path.abspath("./") +'\\data\\'
+                        image_path = data_path + 'DTQ_RP551CPU_ZKXL0200_V0102.bin'
+                        size       = os.path.getsize(image_path)
                         self.emit(SIGNAL('output(QString)'),data) 
-                    if read_char == '.':
-                        ser.write('1')
+                        iap = UartYmodemSendFile()
+                        ser.write(iap.encode_header_package(image_path,size))
+                        down_load_image_flag = 4
+
+                if down_load_image_flag == 4:
+                    #data = u"传输镜像文件..."
+                    print "connect..."
+                    #self.emit(SIGNAL('output(QString)'),data ) 
+
                 else:
                     #print read_char
                     if decode_type_flag == 0:
@@ -67,7 +188,11 @@ class UartListen(QThread):
                         self.emit(SIGNAL('output(QString)'),recv_str) 
                         if down_load_image_flag == 1:
                             down_load_image_flag = 2
-                        #self.sleep(3) 
+                            sleep(0.5) 
+                            ser.write('1')
+                            data = u"尝试建立连接..."
+                            self.emit(SIGNAL('output(QString)'),data )
+                            down_load_image_flag = 3 
 
 class JsonDecode():
     def __init__(self):
@@ -644,8 +769,6 @@ class DtqDebuger(QDialog):
     def uart_download_image(self):
         global down_load_image_flag
 
-        data_path  = os.path.abspath("./") +'\\data\\'
-        image_path = data_path + 'DTQ_RP551CPU_ZKXL0200_V0102.bin'
         self.send_cmd_combo.setCurrentIndex(self.send_cmd_combo.
             findText(u'下载程序'))
         down_load_image_flag = 1
@@ -745,3 +868,4 @@ if __name__=='__main__':
     datdebuger = DtqDebuger()
     datdebuger.show()
     app.exec_()
+
